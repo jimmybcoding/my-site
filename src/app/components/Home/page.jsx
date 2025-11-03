090f570f3f3cc273eb128235f0d12e79c3eb5669
James Barbour <jameskbarbour1@gmail.com>
2025-11-03 09:25:17 -0500

    Prettier/linting

--- src/app/components/Home/page.jsx
+++ src/app/components/Home/page.jsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import { domine } from "@/app/fonts/fonts";
 
-
 const Intro = () => {
   const strings = [
     "Software Developer",
